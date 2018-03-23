470b9498c1124bf17958fbc3d3d9f418f3c94121
Kevin Budd <kevin@kevinbudd.com>
2018-03-23 15:35:37 -0500

    assignment: stacks & queues completed

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -8,11 +8,28 @@ class MyQueue
   end
 
   def enqueue(element)
+    if self.empty?
+      @queue[0] = element
+    else
+      @queue[@queue.length] = element
+    end
+    update_head_tail
   end
 
   def dequeue
+    unless self.empty?
+      item = @queue.delete_at(0)
+      update_head_tail
+    end
+    item
   end
 
   def empty?
+    @queue.length == 0
+  end
+
+  def update_head_tail
+    @head = @queue[0]
+    @tail = @queue[@queue.length - 1]
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,27 @@ class MyStack
   end
 
   def push(item)
+    if self.empty?
+      @stack[0] = item
+    else
+      @stack[@stack.length] = item
+    end
+    self.top = item
   end
 
   def pop
+    unless self.empty?
+      item = @stack.delete_at(@stack.length - 1)
+      self.top = @stack.last
+    end
+    item
   end
 
   def empty?
+    @stack.length == 0
+  end
+
+  def count
+    @stack.length
   end
 end
